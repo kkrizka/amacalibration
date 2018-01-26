a97675275ecdd9918b337c959235c30a6d7645a0
Karol Krizka <kkrizka@gmail.com>
2018-01-25 17:41:37 -0800

    Add pbreport.Reports. Fix labels in OpAmpGain plots.

--- pbreport.py
+++ pbreport.py
@@ -7,6 +7,20 @@ import os.path
 
 from IPython.display import HTML, display
 
+class Reports:
+    def __init__(self,reports):
+        self.names   =[r.name for r in reports]
+
+        self.vin=pd.concat([r.vin for r in reports], ignore_index=True) if len(reports)>0 else None
+        self.ileak=pd.concat([r.ileak for r in reports], ignore_index=True) if len(reports)>0 else None
+
+    def append(self,r):
+        self.names.append(r.name)
+
+        self.vin=pd.concat([r.vin, self.vin], ignore_index=True) if self.vin is not None else r.vin
+        self.ileak=pd.concat([r.ileak, self.ileak], ignore_index=True) if self.ileak is not None else r.ileak
+
+
 class Report:
     def __init__(self,name):
         self.name=name
@@ -41,9 +55,9 @@ class Report:
         plt.semilogx(self.ileak.Ileak,self.ileak.OpAmpGain0,label='OpAmpGain0')
         plt.semilogx(self.ileak.Ileak,self.ileak.OpAmpGain1,label='OpAmpGain1')
         plt.semilogx(self.ileak.Ileak,self.ileak.OpAmpGain2,label='OpAmpGain2')
-        plt.semilogx(self.ileak.Ileak,self.ileak.OpAmpGain4,label='OpAmpGain3')
-        plt.semilogx(self.ileak.Ileak,self.ileak.OpAmpGain8,label='OpAmpGain4')
-        plt.xlabel('Ileak [V]')
+        plt.semilogx(self.ileak.Ileak,self.ileak.OpAmpGain4,label='OpAmpGain4')
+        plt.semilogx(self.ileak.Ileak,self.ileak.OpAmpGain8,label='OpAmpGain8')
+        plt.xlabel('Ileak [mA]')
         plt.ylabel('Ileak [counts]')
         plt.title(self.name)
         plt.legend(frameon=False)
